fc19fd69c39838e587c408c28f4cb26d0d8b6884
Alexsandro Francisco <alexsandro2134@gmail.com>
2021-06-20 11:05:03 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     packages=find_packages() + find_packages(where="./protocols"),
     version="1.0.0",
     description="Creates a network socket to communicate with the VSSS League referee",
+    long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
     author="Project-Neon",
     author_email="projectneon@gmail.com",
